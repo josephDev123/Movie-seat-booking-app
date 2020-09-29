e21822c9450c4acab77ffee36e6e8188d470f76e
uzuegbu joseph <josephuzuegbu55@gmail.com>
2020-09-29 11:04:44 +0100

    Seat booking app files created, saved and added

---
+++ README.md
@@ -0,0 +1,3 @@
+#Movie Seat Booking
+
+Movie seat booking app is a simple app that allows you to select movies from specified movies. It shows the number of selected movies and also available seats in a Cinema.
---
+++ script.js
@@ -0,0 +1,84 @@
+
+
+
+const movieSelector = document.getElementById('movie_select_option');
+let movieSelector_price = +document.getElementById('movie_select_option').value;
+
+const seatContainer = document.querySelector('.move_seats_container');
+const seat = document.querySelectorAll('.row .seat:not(.Occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+
+populate();
+
+
+//storing movie in localstorage
+function movie_storage(movie_index, movie_value){
+
+localStorage.setItem('movieIndex', movie_index);
+localStorage.setItem('movieValue', movie_value);
+
+}
+
+
+//change movie price
+movieSelector.addEventListener('change', (e)=>{
+	movieSelector_price = +e.target.value;
+	movie_storage(e.target.value, e.target.selectedIndex);
+
+	updateCountTotal();
+})
+
+
+
+//updated count and price
+function updateCountTotal(){
+	const selectedSeat = document.querySelectorAll('.row .seat.selected'); 
+	const selectedSeatLength = selectedSeat.length;
+	const seatIndex = [...selectedSeat].map((seatItem)=>{
+		return [...seat].indexOf(seatItem);
+
+	})
+
+	localStorage.setItem('seatSelected', JSON.stringify(seatIndex));
+
+	count.innerHTML = selectedSeatLength;
+	total.innerHTML =selectedSeatLength * movieSelector_price;
+}
+
+//populate UI
+function populate(){
+	const selectedSeats = JSON.parse(localStorage.getItem('seatSelected'));
+
+	if (selectedSeats !==null && selectedSeats.length > 0) {
+		seat.forEach((seats, index)=>{
+			if (selectedSeats.indexOf(index) > -1) {
+				seats.classList.add('selected');
+			}
+		})
+	}
+
+	//getting selected movies index from localstorage
+	const selectedMovieIndex = localStorage.getItem('movieIndex');
+	if (selectedMovieIndex !==null) {
+		movieSelector_price.selectedIndex = selectedMovieIndex;
+	}
+
+}
+
+
+
+//event listener
+seatContainer.addEventListener('click', (e)=>{
+	if(e.target.classList.contains('seat') && !e.target.classList.contains('Occupied')){
+		 e.target.classList.toggle('selected');
+
+		updateCountTotal();
+
+	}
+
+})
+
+
+//initial count
+updateCountTotal();
